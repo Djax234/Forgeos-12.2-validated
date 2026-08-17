9776764d2e07c0dfc11ef5ca3f479fe4e63252a1
Djax234 <little.big.bro1990@gmail.com>
2026-08-17 10:57:56 -0600

    Delete orge-os/scripts directory

--- orge-os/scripts/sign-artifacts.sh
+++
@@ -1,5 +0,0 @@
-# Add to forge-os/scripts/sign-artifacts.sh
-echo "[Forge] Signing Glimmer model for verified boot..."
-find /usr/share/models/muse-glimmer -type f -exec sha256sum {} \; > /usr/share/models/muse-glimmer/MANIFEST
-gpg --detach-sign --armor -u forge@ramaedge /usr/share/models/muse-glimmer/MANIFEST
-# MANIFEST.sig checked by ExecCondition above
